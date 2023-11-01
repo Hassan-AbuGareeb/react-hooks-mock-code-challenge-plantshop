6c0da36103075b8f71ad5e06d6f4632eb3828d80
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-11-01 23:39:06 +0300

    core deliverables done

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ onAddPlant }) {
+  const [plantData, setPlantData] = useState({
+    name: "",
+    image: "",
+    price: "",
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    //add plant to plants state
+    const newPlant = { ...plantData };
+    for (const property in plantData) {
+      setPlantData((prevObj) => {
+        return { ...prevObj, [property]: "" };
+      });
+    }
+
+    //fetch to add plant to the database
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPlant),
+    })
+      .then((resp) => resp.json())
+      .then((data) => onAddPlant(data));
+  }
+
+  function handleChange(event) {
+    const key = event.target.name;
+    const value = event.target.value;
+    setPlantData({ ...plantData, [key]: value });
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Plant name"
+          value={plantData.name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="image"
+          placeholder="Image URL"
+          value={plantData.image}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="price"
+          step="0.01"
+          placeholder="Price"
+          value={plantData.price}
+          onChange={handleChange}
+        />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,25 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
-  return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
+function PlantList({ search, plants }) {
+  const searchedPlants = plants.filter((plant) => {
+    if (plant.name.toLowerCase().trim().includes(search.toLowerCase().trim()))
+      return true;
+    return false;
+  });
+
+  const plantItems = searchedPlants.map((plant) => {
+    return (
+      <PlantCard
+        key={plant.id}
+        name={plant.name}
+        image={plant.image}
+        price={plant.price}
+      />
+    );
+  });
+
+  return <ul className="cards">{plantItems}</ul>;
 }
 
 export default PlantList;
